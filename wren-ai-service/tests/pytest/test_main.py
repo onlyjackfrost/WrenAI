fd7b26819baa557d2555b4732e1b348440ea543f
ChihYu Yeh <chihyu.jimmy.yeh@gmail.com>
2024-10-18 11:38:36 +0800

    fix conflict

--- wren-ai-service/tests/pytest/test_main.py
+++ wren-ai-service/tests/pytest/test_main.py
@@ -2,79 +2,165 @@ import json
 import uuid
 
 import orjson
+import pytest
 from fastapi.testclient import TestClient
+from pytest_lazy_fixtures import lf
 
 from src.__main__ import app
 
 GLOBAL_DATA = {
-    "semantics_preperation_id": str(uuid.uuid4()),
+    "mdl_hash": str(uuid.uuid4()),
     "query_id": None,
 }
 
 
-def test_semantics_preparations():
+@pytest.fixture
+def valid_mdl():
+    with open("tests/data/book_2_mdl.json", "r") as f:
+        return orjson.dumps(json.load(f)).decode("utf-8")
+
+
+@pytest.fixture
+def invalid_mdl():
+    return "invalid-mdl"
+
+
+@pytest.fixture
+def valid_semantics_preparation_data(
+    valid_mdl: str,
+):
+    return {
+        "mdl": valid_mdl,
+        "mdl_hash": GLOBAL_DATA["mdl_hash"],
+    }
+
+
+@pytest.fixture
+def invalid_mdl_semantics_preparation_data(
+    invalid_mdl: str,
+):
+    return {
+        "mdl": invalid_mdl,
+        "mdl_hash": str(uuid.uuid4()),
+    }
+
+
+@pytest.fixture
+def no_mdl_semantics_preparation_data():
+    return {
+        "mdl_hash": str(uuid.uuid4()),
+    }
+
+
+@pytest.fixture
+def no_mdl_hash_semantics_preparation_data(
+    valid_mdl: str,
+):
+    return {
+        "mdl": valid_mdl,
+    }
+
+
+@pytest.mark.parametrize(
+    "data,status_code,expected_status",
+    [
+        pytest.param(
+            lf("valid_semantics_preparation_data"),
+            200,
+            "finished",
+            marks=pytest.mark.dependency(name="valid_semantics_prep"),
+        ),
+        (lf("invalid_mdl_semantics_preparation_data"), 200, "failed"),
+        (lf("no_mdl_semantics_preparation_data"), 400, None),
+        (lf("no_mdl_hash_semantics_preparation_data"), 400, None),
+    ],
+)
+@pytest.mark.dependency()
+def test_semantics_preparations(data, status_code, expected_status):
     with TestClient(app) as client:
-        semantics_preperation_id = GLOBAL_DATA["semantics_preperation_id"]
-
-        with open("tests/data/book_2_mdl.json", "r") as f:
-            mdl_str = orjson.dumps(json.load(f)).decode("utf-8")
-
         response = client.post(
             url="/v1/semantics-preparations",
-            json={
-                "mdl": mdl_str,
-                "id": semantics_preperation_id,
-            },
+            json=data,
         )
 
-        assert response.status_code == 200
-        assert response.json()["id"] == semantics_preperation_id
-
-        status = "indexing"
-
-        while status == "indexing":
-            response = client.get(
-                url=f"/v1/semantics-preparations/{semantics_preperation_id}/status"
-            )
-
+        if "mdl" in data and "mdl_hash" in data:
             assert response.status_code == 200
-            assert response.json()["status"] in ["indexing", "finished", "failed"]
-            status = response.json()["status"]
+            assert response.json()["id"] == data["mdl_hash"]
 
-        assert status == "finished"
+            status = "indexing"
+
+            while status == "indexing":
+                response = client.get(
+                    url=f"/v1/semantics-preparations/{data["mdl_hash"]}/status"
+                )
+
+                assert response.status_code == status_code
+                assert response.json()["status"] in ["indexing", "finished", "failed"]
+                status = response.json()["status"]
+
+            assert status == expected_status
+        else:
+            assert response.status_code == status_code
 
 
-def test_asks_with_successful_query():
+@pytest.fixture
+def valid_ask_data():
+    return {
+        "query": "how many books",
+        "mdl_hash": GLOBAL_DATA["mdl_hash"],
+    }
+
+
+@pytest.fixture
+def invalid_ask_data():
+    return {
+        "query": "xxxxxx",
+        "mdl_hash": GLOBAL_DATA["mdl_hash"],
+    }
+
+
+@pytest.fixture
+def no_query_ask_data():
+    return {
+        "mdl_hash": GLOBAL_DATA["mdl_hash"],
+    }
+
+
+@pytest.mark.parametrize(
+    "data,status_code,expected_status",
+    [
+        (lf("valid_ask_data"), 200, "finished"),
+        (lf("invalid_ask_data"), 200, "failed"),
+        (lf("no_query_ask_data"), 400, None),
+    ],
+)
+@pytest.mark.dependency(depends=["valid_semantics_prep"])
+def test_asks(data, status_code, expected_status):
     with TestClient(app) as client:
-        semantics_preparation_id = GLOBAL_DATA["semantics_preperation_id"]
-
         response = client.post(
             url="/v1/asks",
-            json={
-                "query": "How many books are there?",
-                "id": semantics_preparation_id,
-            },
+            json=data,
         )
 
-        assert response.status_code == 200
-        assert response.json()["query_id"] != ""
+        if "query" in data and "mdl_hash" in data:
+            assert response.status_code == 200
+            assert response.json()["query_id"] != ""
 
-        query_id = response.json()["query_id"]
-        GLOBAL_DATA["query_id"] = query_id
+            query_id = response.json()["query_id"]
+            GLOBAL_DATA["query_id"] = query_id
 
-        response = client.get(url=f"/v1/asks/{query_id}/result")
-        while (
-            response.json()["status"] != "finished"
-            and response.json()["status"] != "failed"
-        ):
             response = client.get(url=f"/v1/asks/{query_id}/result")
+            while (
+                response.json()["status"] != "finished"
+                and response.json()["status"] != "failed"
+            ):
+                response = client.get(url=f"/v1/asks/{query_id}/result")
 
-        # TODO: we'll refactor almost all test case with a mock server, thus temporarily only assert the status is finished or failed.
-        assert response.status_code == 200
-        assert response.json()["status"] == "finished" or "failed"
-        # for r in response.json()["response"]:
-        #     assert r["sql"] is not None and r["sql"] != ""
-        #     assert r["summary"] is not None and r["summary"] != ""
+            # TODO: we'll refactor almost all test case with a mock server, thus temporarily only assert the status is finished or failed.
+            assert response.status_code == status_code
+            assert response.json()["status"] == expected_status
+        else:
+            assert response.status_code == status_code
 
 
 def test_stop_asks():
